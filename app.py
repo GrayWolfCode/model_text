16140b1e2022283301dc6ca48e87a83f508ccd18
GrayWolfCode <jlee888624@gmail.com>
2023-10-03 09:32:54 +0900

    awww

--- app.py
+++ app.py
@@ -20,5 +20,15 @@ def split_text():
         return jsonify(error="No text provided"), 400
 
 
+@app.route('/combine_text', methods=['POST'])
+def combine_text():
+    data = request.json
+    if data and 'key1' in data and 'key2' in data:
+        combined_text = data['key1'] + "#" + data['key2']
+        return jsonify(combined_text=combined_text)
+    else:
+        return jsonify(error="Both key1 and key2 must be provided"), 400
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
